7f8816e2e780fbd0db73e4c42f68a138002f5319
github <pragya.sharma2_cs19@gla.ac.in>
2021-10-05 19:50:14 +0530

    Initial commit

---
+++ app/src/main/java/com/example/sensorsapp/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.sensorsapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
