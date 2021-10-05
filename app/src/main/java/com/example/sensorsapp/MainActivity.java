1dfc751d69021bb2991ac578f968c834687cbdaa
github <pragya.sharma2_cs19@gla.ac.in>
2021-10-05 19:57:13 +0530

    declare reference variables.

--- app/src/main/java/com/example/sensorsapp/MainActivity.java
+++ app/src/main/java/com/example/sensorsapp/MainActivity.java
@@ -2,9 +2,21 @@ package com.example.sensorsapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.bluetooth.BluetoothAdapter;
+import android.hardware.Sensor;
+import android.hardware.SensorManager;
+import android.media.MediaPlayer;
+import android.net.wifi.WifiManager;
 import android.os.Bundle;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
+    ImageView imageView;
+    SensorManager sensorManager;
+    Sensor sensor;
+    BluetoothAdapter bluetoothAdapter;
+    WifiManager wifiManager;
+    MediaPlayer mediaPlayer;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
